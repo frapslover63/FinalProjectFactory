22b3f58294f7ec7fdd28e124ca5f7d3e345c2187
hawkjstn98 <hawk.jstn98@gmail.com>
2019-04-29 20:47:09 +0700

    Interface class

---
+++ app/src/main/java/data/Data.kt
@@ -0,0 +1,34 @@
+package data
+
+interface Url{
+    fun UrlGetStock(): String{
+        return baseUrl()+"/getData/getstockPabrik.php";
+    }
+
+    fun callUrlTrigger(): String{
+        return baseUrl()+"/transactionData/startTransactionPabrik.php";
+    }
+
+    fun callUrlTransaction(): String{
+        return baseUrl()+"/transactionData/transactionPabrik.php";
+    }
+}
+
+interface CustomParameter{
+    fun AuthParam(): String{
+        return "?userName=MarioWibu&password=A559C7CA6258E603E59125333FFF381496982D96CFB54B691A5E84DBFDF2B475";
+    }
+
+    fun StartTransactionParam(): String{
+        return "?userName=MWB&Password=MWB";
+    }
+
+    fun TransactionParam(id: String, data:String): String{
+        return "?transactionid="+id+"&jsonString="+data;
+    }
+}
+
+
+private fun baseUrl(): String{
+    return "http://192.168.1.6";
+}
--- app/src/main/java/id/ac/umn/finalprojectfactory/ListBarangActivity.kt
+++ app/src/main/java/id/ac/umn/finalprojectfactory/ListBarangActivity.kt
@@ -8,5 +8,7 @@ class ListBarangActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_barang)
+
+
     }
 }
