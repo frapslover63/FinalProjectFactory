b3e88badb42b28c6d3bc02138a5ad2568f3aa9f2
Mario Alexander <39822647+Xtinguir@users.noreply.github.com>
2019-05-04 22:32:01 +0700

    Refactor

--- app/src/main/java/id/ac/umn/finalprojectfactory/TransaksiPabrikConfirmActivity.kt
+++ app/src/main/java/id/ac/umn/finalprojectfactory/TransaksiPabrikConfirmActivity.kt
@@ -3,10 +3,10 @@ package id.ac.umn.finalprojectfactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class TransaksiPabConfirmActivity : AppCompatActivity() {
+class TransaksiPabrikConfirmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_transaction_fac_confirm)
+        setContentView(R.layout.activity_transaksi_pabrik_conf)
     }
 }
